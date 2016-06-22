0fda674e2be4b35f5ea70eacda194e38bfcbc81a
Michael Melvin <michael.melvin@panasonic.aero>
2016-06-22 15:51:38 -0700

    Install both ember-browerify + urljs in parent app

--- blueprints/ember-cli-mocha-reporter/index.js
+++ blueprints/ember-cli-mocha-reporter/index.js
@@ -1,14 +1,17 @@
 module.exports = {
 
-    name: 'ember-cli-mocha-reporter',
-
-    normalizeEntityName: function () {},
+    normalizeEntityName: function (entityName) {
+        return entityName;
+    },
 
     afterInstall: function () {
         // even with ember-browserify installed in both this addon and its parent
         // it can't find the urljs library unless urljs is also installed in the parent
         // see https://github.com/ef4/ember-browserify/issues/38
-        return this.addPackageToProject('urljs', '^2.3.1');
+        return this.addPackagesToProject([
+            {name: 'ember-browserify', target: '^1.1.9'},
+            {name: 'urljs', target: '^2.3.1'}
+        ]);
     }
 
 };
