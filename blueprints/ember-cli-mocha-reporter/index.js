eeb3992c3184ab6bd245e4313879993abe69e35e
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2016-06-30 17:15:47 +0300

    Import url.js from Bower instead of Browserify for smoother experience

--- blueprints/ember-cli-mocha-reporter/index.js
+++ blueprints/ember-cli-mocha-reporter/index.js
@@ -5,13 +5,7 @@ module.exports = {
     },
 
     afterInstall: function () {
-        // even with ember-browserify installed in both this addon and its parent
-        // it can't find the urljs library unless urljs is also installed in the parent
-        // see https://github.com/ef4/ember-browserify/issues/38
-        return this.addPackagesToProject([
-            {name: 'ember-browserify', target: '^1.1.9'},
-            {name: 'urljs', target: '^2.3.1'}
-        ]);
+        return this.addBowerPackageToProject('url.js', 'jillix/url.js#^2.4.0');
     }
 
 };
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports = {
 
     included: function (app) {
         if (app.tests) {
+            app.import('bower_components/url.js/src/url.min.js',                    {type: 'test'});
             app.import('vendor/ember-cli-mocha-reporter/test-container-styles.css', {type: 'test'});
         }
     }
